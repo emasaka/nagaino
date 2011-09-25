62817bccb0c87f0b7b46e91ca7bd78c7c7174f07
emasaka <emasaka@gmail.com>
2011-09-26 00:18:52 +0900

    POST API

--- src/nagaino/core.clj
+++ src/nagaino/core.clj
@@ -1,7 +1,9 @@
 (ns nagaino.core
   (:use [compojure.core]
 	[ring.adapter.jetty]
+	[ring.util.codec :only [url-decode]]
 	[clojure.contrib.json :only [json-str]]
+	[clojure.contrib.str-utils :only [re-split]]
 	[nagaino.expandurls :only [expand-urls]] )
   (:require [compojure.handler :as handler]
 	    [compojure.route :as route] ))
@@ -16,6 +18,9 @@
 (defroutes route
   (GET "/api/v0/expand" [:as params]
        (-> params query->longurl json-res) )
+  (POST "/api/v0/expandText" {params :params}
+	(->> params :shortUrls url-decode (re-split #"\n") expand-urls
+	     json-res ))
   (route/resources "/") )
 
 (def app (handler/api route))
