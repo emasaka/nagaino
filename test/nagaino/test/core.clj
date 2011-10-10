365d98e763294ada406fdfcde5e0226da7125eb9
emasaka <emasaka@gmail.com>
2011-10-10 13:14:22 +0900

    some tests

--- test/nagaino/test/core.clj
+++ test/nagaino/test/core.clj
@@ -1,6 +1,15 @@
 (ns nagaino.test.core
-  (:use [nagaino.core])
-  (:use [clojure.test]))
+  (:use [nagaino.core]
+	[clojure.test] ))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(deftest test-transform-result
+  (let [urls [{:short_url "short1" :long_url "long1"}
+	      {:short_url "short2" :long_url "long2"}
+	      {:short_url "short3" :long_url "long3"} ]]
+    (is (= (transform-result "json_hash" urls)
+	   {"short1" {:short_url "short1" :long_url "long1"}
+	    "short2" {:short_url "short2" :long_url "long2"}
+	    "short3" {:short_url "short3" :long_url "long3"}} ))
+    (is (= (transform-result "json_hash_simple" urls)
+	   {"short1" "long1", "short2" "long2", "short3" "long3"} ))
+    (is (= (transform-result "json" urls) urls)) ))
