15a2603a8c944d8c33ac6c5d2a233af1620e4ee0
emasaka <emasaka@gmail.com>
2012-01-05 13:34:19 +0900

    add 1 host to shorturl-hosts

--- resources/config.clj
+++ resources/config.clj
@@ -3,7 +3,8 @@
 		  "http://tr.im/" "http://short.to/" "http://ow.ly/"
 		  "http://u.nu/" "http://twurl.nl/" "http://icio.us/"
 		  "http://htn.to/" "http://cot.ag/" "http://ht.ly/"
-		  "http://p.tl/" "http://url4.eu/" "http://ur1.ca/" )
+		  "http://p.tl/" "http://url4.eu/" "http://ur1.ca/"
+                  "http://sgp.cm/" )
  :bitly-hosts ("http://bit.ly/" "http://amzn.to/" "http://s.nikkei.com/"
 	       "http://t.asahi.com/" "http://nyti.ms/" "http://tcrn.ch/"
 	       "http://huff.to/" "http://on.cnn.com/")
