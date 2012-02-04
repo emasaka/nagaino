2884147b91c7ef52026d68325db4939681232243
emasaka <emasaka@gmail.com>
2012-02-04 13:46:40 +0900

    versioned up libraries

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :dependencies [[org.clojure/clojure "1.2.1"]
 		 [org.clojure/clojure-contrib "1.2.0"]
 		 [clojure-http-client/clojure-http-client "1.1.0"]
-                 [compojure "1.0.0"]
+                 [compojure "1.0.1"]
                  [hiccup "0.3.8"]
-                 [ring "1.0.1"]
-                 [congomongo "0.1.4-SNAPSHOT"] ]
+                 [ring "1.0.2"]
+                 [congomongo "0.1.7"] ]
   :dev-dependencies [[swank-clojure "1.3.4"]]
   :main nagaino.core )
