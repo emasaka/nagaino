0e4c09b68d4be3a7f32114b13d923a1b217cab52
emasaka <emasaka@gmail.com>
2012-05-30 09:30:21 +0900

    Clojure 1.4.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject nagaino "0.0.1-SNAPSHOT"
   :description "bulk expander of shortened URLs"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [clj-http "0.3.1"]
                  [compojure "1.0.1"]
                  [hiccup "0.3.8"]
