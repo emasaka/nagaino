8144d4f3e33bcfb7f8629efede4e8a4a4747b600
emasaka <emasaka@gmail.com>
2014-10-14 12:37:41 +0900

    updated versions of libraries

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
 (defproject nagaino "0.0.1-SNAPSHOT"
   :description "bulk expander of shortened URLs"
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [clj-http "0.4.1"]
-                 [compojure "1.0.1"]
-                 [hiccup "1.0.0"]
-                 [ring "1.1.0"]
-                 [congomongo "0.1.7"] ]
-  :dev-dependencies [[swank-clojure "1.3.4"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [clj-http "1.0.0"]
+                 [compojure "1.2.0"]
+                 [hiccup "1.0.5"]
+                 [ring "1.3.1"]
+                 [congomongo "0.4.4"] ]
   :main nagaino.core )
