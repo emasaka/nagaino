b720e374e6c94f5149781715872edb93243d9f85
emasaka <emasaka@gmail.com>
2014-11-16 22:11:20 +0900

    added itt.tt

--- resources/config.clj
+++ resources/config.clj
@@ -5,7 +5,7 @@
 		  "http://htn.to/" "http://cot.ag/" "http://ht.ly/"
 		  "http://p.tl/" "http://url4.eu/" "http://ur1.ca/"
                   "http://sgp.cm/" "http://dlvr.it/" "http://me.lt/"
-                  "http://j.plustar.jp/" )
+                  "http://j.plustar.jp/" "http://ift.tt/" )
  :bitly-hosts ("http://bit.ly/" "http://amzn.to/" "http://s.nikkei.com/"
 	       "http://t.asahi.com/" "http://nyti.ms/" "http://tcrn.ch/"
 	       "http://huff.to/" "http://on.cnn.com/" "http://e8.biz/"
