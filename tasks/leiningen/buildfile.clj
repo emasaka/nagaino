45518e8c86728723f78a1dc2798dbdacb377366b
emasaka <emasaka@gmail.com>
2014-11-23 13:08:48 +0900

    minify js file

--- tasks/leiningen/buildfile.clj
+++ tasks/leiningen/buildfile.clj
@@ -1,7 +1,8 @@
 (ns leiningen.buildfile
-  (:use [cemerick.pomegranate :only [add-dependencies]])
-  (:require [clojure.string]
-            [clojure.java.io :as io] ))
+  (:refer-clojure :exclude [replace])
+  (:use [cemerick.pomegranate :only [add-dependencies]]
+        [clojure.string :only [replace]] )
+  (:require [clojure.java.io :as io]) )
 
 (add-dependencies
  :coordinates '[[me.geso/regexp-trie "0.1.10"]]
@@ -13,9 +14,9 @@
   (str "^"
        (-> (.regexp (reduce (fn [r v] (.add r v) r) (RegexpTrie.) urls))
            ;; quoting `.' and `/' in regexp by string/replace is workaround
-           (clojure.string/replace #"[/.]" "\\\\$0")
+           (replace #"[/.]" "\\\\$0")
            ;; JavaScript regexp doesn't handle `\Q..\E' (workaround)
-           (clojure.string/replace "\\Q:\\E" ":") )))
+           (replace "\\Q:\\E" ":") )))
 
 (defn read-url-conf []
   (let [conf (read-string (slurp "resources/config.clj"))]
@@ -23,13 +24,26 @@
 
 (defn apply-template [tmpl dat]
   ;; workaround
-  (clojure.string/replace tmpl "{{URL_RE}}" (:URL_RE dat)) )
+  (replace tmpl "{{URL_RE}}" (:URL_RE dat)) )
+
+(defn minify-js [dat]
+  ;; workaround.  Be careful about string literals
+  (-> dat
+      (replace #" //[^\n]*" "")         ; remove comments
+      (replace #"\s{2,}" " ")           ; compress spaces
+      (replace #" (=|==|\+|<|>|&&|\|\|) (?!\!)" "$1")
+      (replace #"([,!]) " "$1")
+      (replace #"([;{}]) " "$1")
+      (replace " {" "{")
+      (replace #"([()]) ([()])" "$1$2")
+      (replace #"(if|for) \(" "$1(") ))
 
 (defn build-js []
   (.mkdirs (io/file "resources/public/js"))
   (spit "resources/public/js/nagainolet.js"
-        (apply-template (slurp "resources/templates/js/nagainolet.js")
-                        {:URL_RE (make-url-re (read-url-conf))} )))
+        (-> (slurp "resources/templates/js/nagainolet.js")
+            (apply-template {:URL_RE (make-url-re (read-url-conf))})
+            (minify-js) )))
 
 (defn buildfile [project]
   (build-js) )
