5e6c0eaf2bf83ce6cdf06619a6541f7a428f6ed0
emasaka <emasaka@gmail.com>
2014-12-03 23:06:43 +0900

    j.mp is bit.ly

--- resources/config.clj
+++ resources/config.clj
@@ -1,13 +1,14 @@
-{:shorturl-hosts ("http://t.co/" "https://t.co/" "http://tinyurl.com/" "http://is.gd/" "http://wp.me/"
-		  "http://ff.im/" "http://j.mp/" "http://goo.gl/"
+{:shorturl-hosts ("http://t.co/" "https://t.co/" "http://tinyurl.com/"
+                  "http://is.gd/" "http://wp.me/"
+		  "http://ff.im/" "http://goo.gl/"
 		  "http://tr.im/" "http://short.to/" "http://ow.ly/"
 		  "http://u.nu/" "http://twurl.nl/" "http://icio.us/"
 		  "http://htn.to/" "http://cot.ag/" "http://ht.ly/"
 		  "http://p.tl/" "http://url4.eu/" "http://ur1.ca/"
                   "http://sgp.cm/" "http://dlvr.it/" "http://me.lt/"
                   "http://j.plustar.jp/" "http://ift.tt/" )
- :bitly-hosts ("http://bit.ly/" "http://amzn.to/" "http://s.nikkei.com/"
-	       "http://t.asahi.com/" "http://nyti.ms/" "http://tcrn.ch/"
-	       "http://huff.to/" "http://on.cnn.com/" "http://e8.biz/"
-	       "http://reut.rs/" "http://nkbp.jp/" )
+ :bitly-hosts ("http://bit.ly/" "http://j.mp/" "http://amzn.to/"
+               "http://s.nikkei.com/" "http://t.asahi.com/" "http://nyti.ms/"
+               "http://tcrn.ch/" "http://huff.to/" "http://on.cnn.com/"
+               "http://e8.biz/" "http://reut.rs/" "http://nkbp.jp/" )
  }
