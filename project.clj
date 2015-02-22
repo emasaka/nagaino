f36b8a8a741c7b36f8637d97ae11a82dec78d2d4
emasaka <emasaka@gmail.com>
2015-02-22 13:18:01 +0900

    updated libraries

--- project.clj
+++ project.clj
@@ -2,16 +2,16 @@
   :description "bulk expander of shortened URLs"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [clj-http "1.0.1"]
-                 [compojure "1.3.1"]
+                 [compojure "1.3.2"]
                  [hiccup "1.0.5"]
                  [ring "1.3.2"]
-                 [ring/ring-defaults "0.1.2"]
+                 [ring/ring-defaults "0.1.4"]
                  [ring/ring-servlet "1.3.2"]
                  [congomongo "0.4.4"]
                  [org.clojure/tools.logging "0.3.1"]
                  [me.geso/regexp-trie "0.1.10"] ]
   :profiles {:dev {:dependencies [[ring-mock "0.1.5"]]}}
-  :plugins [[lein-ring "0.8.13"]]
+  :plugins [[lein-ring "0.9.1"]]
   :ring {:handler nagaino.core/app}
   :clean-targets ^{:protect false} [:target-path
                                     "resources/public/js/nagainolet.js" ]
