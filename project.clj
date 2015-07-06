7895e2bc0600817b08d228dc451b9b2dc96abb52
emasaka <emasaka@gmail.com>
2015-07-06 20:31:45 +0900

    updated versions of libraries
    compojure
    regexp-trie
    lein-ring

--- project.clj
+++ project.clj
@@ -2,16 +2,16 @@
   :description "bulk expander of shortened URLs"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [clj-http "1.0.1"]
-                 [compojure "1.3.2"]
+                 [compojure "1.3.4"]
                  [hiccup "1.0.5"]
                  [ring "1.3.2"]
                  [ring/ring-defaults "0.1.4"]
                  [ring/ring-servlet "1.3.2"]
                  [congomongo "0.4.4"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [me.geso/regexp-trie "0.1.10"] ]
+                 [me.geso/regexp-trie "1.0.2"] ]
   :profiles {:dev {:dependencies [[ring-mock "0.1.5"]]}}
-  :plugins [[lein-ring "0.9.1"]]
+  :plugins [[lein-ring "0.9.6"]]
   :ring {:handler nagaino.core/app}
   :clean-targets ^{:protect false} [:target-path
                                     "resources/public/js/nagainolet.js" ]
