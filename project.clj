e8da7328c2a2c13a32789b88b6c69676721894b0
emasaka <emasaka@gmail.com>
2016-01-28 09:06:36 +0900

    updated versions of libraries
    * clj-http
    * congomongo
    
    and explicitly defined cheshire for clj-http.

--- project.clj
+++ project.clj
@@ -1,12 +1,13 @@
 (defproject nagaino "0.0.1-SNAPSHOT"
   :description "bulk expander of shortened URLs"
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [clj-http "1.0.1"]
+                 [clj-http "2.0.1"]
+                 [cheshire "5.5.0"]
                  [compojure "1.4.0"]
                  [hiccup "1.0.5"]
                  [ring "1.4.0"]
                  [ring/ring-defaults "0.1.5"]
-                 [congomongo "0.4.6"]
+                 [congomongo "0.4.7"]
                  [org.clojure/tools.logging "0.3.1"]
                  [me.geso/regexp-trie "1.0.5"] ]
   :profiles {:dev {:dependencies [[ring/ring-mock "0.3.0"]]}}
